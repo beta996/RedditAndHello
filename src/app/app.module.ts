d761b7b221cfdda93957f429a284ddfb48a0aeea
pc <beata.jackowska99@gmail.com>
2020-03-30 18:07:55 +0200

    Ready version of Reddit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { ArticleComponent } from './article/article.component';
 @NgModule({
   declarations: [
+
+    HelloWorldComponent,
+    ArticleComponent,
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
